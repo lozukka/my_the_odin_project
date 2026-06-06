7642d8f2d2c457c146e8a4bd8664238b72ab0518
Lotta <lotsulotsu@gmail.com>
2026-06-06 15:34:35 +0300

    funcionality to push new books to array from form

--- library/script.js
+++ library/script.js
@@ -14,3 +14,22 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
+
+form.addEventListener("submit", (e) => {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+
+  addBookToLibrary(title, author, pages, read);
+  form.classList.add("hidden");
+  form.reset();
+});
