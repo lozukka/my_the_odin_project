79d930221cd4be9f7c790493eeeae268bddd67da
Lotta <lotsulotsu@gmail.com>
2026-06-06 15:20:06 +0300

    added functionality to "add new book" button

--- library/script.js
+++ library/script.js
@@ -1,1 +1,16 @@
+let myLibrary = [];
+const addBtn = document.getElementById("add-btn");
+const form = document.getElementById("new-book");
+const myLibraryTable = document.getElementById("my-library-table");
 
+addBtn.addEventListener("click", (e) => {
+  form.classList.remove("hidden");
+});
+
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
