ef0d9e18f583533499f4941c521ed76b2323ad91
Lotta <lotsulotsu@gmail.com>
2026-06-15 14:00:56 +0300

    functions for fibonacci

--- recursion/script.js
+++ recursion/script.js
@@ -1,1 +1,27 @@
-console.log("hello");
+//Fibonacci
+// function fibs that returns an array containing Fibonacci sequence. Example input 8 = [0, 1, 1, 2, 3, 5, 8, 13]
+
+function fibs(n) {
+  const sequence = [0, 1];
+  for (let i = 0; i < n - 2; i++) {
+    const sumPair = sequence.slice(-2);
+    sequence.push(sumPair[0] + sumPair[1]);
+    console.log(sequence);
+  }
+}
+
+// same with recursively
+function fibsRec(n) {
+  if (n < 1) {
+    return;
+  }
+  if (n === 1) {
+    return [0];
+  }
+  if (n === 2) {
+    return [0, 1];
+  }
+  const result = fibsRec(n - 1);
+  result.push(result.at(-1) + result.at(-2));
+  return result;
+}
